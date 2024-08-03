8e799e9e664ffb7b0b61e42531e272cfabc92347
omdxp <omarbelghaouti@gmail.com>
2024-08-03 13:06:34 +0200

    expression precedence

--- compiler.h
+++ compiler.h
@@ -256,4 +256,14 @@ void node_push(struct node *node);
 void node_set_vector(struct vector *vec, struct vector *root_vec);
 struct node *node_create(struct node *_node);
 struct node *node_peek_expressionable_or_null();
+
+#define TOTAL_OPERATOR_GROUPS 14
+#define MAX_OPERATORS_IN_GROUP 12
+
+enum { ASSOCIATIVITY_LEFT_TO_RIGHT, ASSOCIATIVITY_RIGHT_TO_LEFT };
+
+struct expressionable_op_precedence_group {
+  char *operators[MAX_OPERATORS_IN_GROUP];
+  int associativity;
+};
 #endif
---
+++ expressionable.c
@@ -0,0 +1,29 @@
+#include "compiler.h"
+
+/**
+ * Format: { "operator1", "operator2", ... , NULL }
+ */
+struct expressionable_op_precedence_group op_precedence[TOTAL_OPERATOR_GROUPS] =
+    {{.operators = {"++", "--", "()", "[]", "(", "[", ".", "->", NULL},
+      .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"*", "/", "%", NULL},
+      .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"+", "-", NULL},
+      .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"<<", ">>", NULL},
+      .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"<", "<=", ">", ">=", NULL},
+      .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"==", "!=", NULL},
+      .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"&", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"^", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"|", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"&&", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"||", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT},
+     {.operators = {"?", ":", NULL},
+      .associativity = ASSOCIATIVITY_RIGHT_TO_LEFT},
+     {.operators = {"=", "+=", "-=", "*=", "/=", "%=", "<<=", ">>=", "&=", "^=",
+                    "|=", NULL},
+      .associativity = ASSOCIATIVITY_RIGHT_TO_LEFT},
+     {.operators = {",", NULL}, .associativity = ASSOCIATIVITY_LEFT_TO_RIGHT}};
