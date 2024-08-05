2dd7b42bd599c6aebd9a1eabce10452bd9ee0306
omdxp <omarbelghaouti@gmail.com>
2024-08-05 19:38:22 +0200

    ignore int abbreviations in data types

--- parser.c
+++ parser.c
@@ -421,9 +421,32 @@ void parse_datatype(struct datatype *dtype) {
   parse_datatype_modifiers(dtype);
 }
 
+bool parser_is_int_valid_after_datatype(struct datatype *dtype) {
+  return dtype->type == DATA_TYPE_LONG || dtype->type == DATA_TYPE_FLOAT ||
+         dtype->type == DATA_TYPE_DOUBLE;
+}
+
+// long int x; // int can be ignored
+void parser_ignore_int(struct datatype *dtype) {
+  if (!token_is_keyword(token_peek_next(), "int")) {
+    // no int to ignore
+    return;
+  }
+
+  if (!parser_is_int_valid_after_datatype(dtype)) {
+    compiler_error(current_process, "Invalid int after datatype");
+  }
+
+  // ignore the int token
+  token_next();
+}
+
 void parse_variable_function_or_struct_union(struct history *history) {
   struct datatype dtype;
   parse_datatype(&dtype);
+
+  // ignore int abbreviation if necessary (.e.g long int x becomes long x)
+  parser_ignore_int(&dtype);
 }
 
 void parse_keyword(struct history *history) {
