d6bf45d15088365a5183ae99675f4f273103a8e8
omdxp <omarbelghaouti@gmail.com>
2024-08-25 15:59:06 +0200

    codegen global variables

--- test.c
+++ test.c
@@ -1,15 +1,2 @@
-struct book book;
-
-struct book {
-  char title[50];
-  char author[50];
-  char subject[100];
-  int book_id;
-};
-
-int test(char *fmt) { return 1; }
-
-int main() {
-  struct book *books;
-  return test(56, books[0].title, 100);
-}
+int x;
+int y;
